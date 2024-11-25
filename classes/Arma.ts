c2eb54fd34875729167a6e45d6c71012c85ff133
nataliaclem <nataliaclementinosilva@gamil.com>
2024-11-25 20:53:24 -0300

    primeira classe

---
+++ classes/Arma.ts
@@ -0,0 +1,23 @@
+export default class Arma {
+    private readonly nome: string = ''
+    private dano: number = 0
+    private readonly descricao: string = ''
+
+    constructor(nome: string, descricao: string, dano: number = 0) {
+        this.nome = nome
+        this.descricao = descricao
+        this.dano = dano
+    }
+
+    getNome() {return this.nome}
+    getDano() {return this.dano}
+    getDescricao() {return this.descricao}
+
+    setDano(dano: number){
+        if(dano < 0) {
+            console.log('Dano não pode ser negativo')
+        } else {
+            this.dano = dano
+        }
+    }
+}
