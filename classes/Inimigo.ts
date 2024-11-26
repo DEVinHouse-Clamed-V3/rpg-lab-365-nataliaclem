d66ea7e64b568e21d6745ffe1e2cae16482fc63e
nataliaclem <nataliaclementinosilva@gamil.com>
2024-11-25 22:11:37 -0300

    atualizacao inimigo

--- classes/Inimigo.ts
+++ classes/Inimigo.ts
@@ -1,14 +1,24 @@
 import Personagem from "./Personagem";
 
 export default class Inimigo extends Personagem {
-    
-    atacar(personagem: Personagem){
-        const chanceDeAcerto = Math.random(); 
-        
-        if (chanceDeAcerto < 0.5)  {
-            console.log("ataque falhou")
-        } else {
-            personagem.receberDano(this.causarDano())
-        }
+  atacar(personagem: Personagem) {
+    const chanceDeAcerto = Math.random();
+    if (chanceDeAcerto < 0.5) {
+      console.log("atacque falhou");
+    } else {
+      personagem.receberDano(this.causarDano());
     }
+  }
+
+  // Método que define uma ação aleatória do inimigo durante o combate
+  comportamentoAleatorio(personagem: Personagem) {
+    const acao = Math.random();
+
+    if (acao < 0.5) {
+      console.log("decide esperar e observar...");
+    } else {
+      console.log("decide atacar!");
+      this.atacar(personagem);
+    }
+  }
 }
