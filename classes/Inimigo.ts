ad22b4865caf80a26767c8b1779187a9433d47fe
nataliaclem <nataliaclementinosilva@gamil.com>
2024-11-25 21:02:11 -0300

    classe inimigo

---
+++ classes/Inimigo.ts
@@ -0,0 +1,14 @@
+import Personagem from "./Personagem";
+
+export default class Inimigo extends Personagem {
+    
+    atacar(personagem: Personagem){
+        const chanceDeAcerto = Math.random(); 
+        
+        if (chanceDeAcerto < 0.5)  {
+            console.log("ataque falhou")
+        } else {
+            personagem.receberDano(this.causarDano())
+        }
+    }
+}
