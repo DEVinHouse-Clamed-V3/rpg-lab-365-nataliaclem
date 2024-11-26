bd637b7045f7899d15ecfdd070b72c78192e1bec
nataliaclem <nataliaclementinosilva@gamil.com>
2024-11-25 22:06:44 -0300

    classe index.ts

--- classes/Personagem.ts
+++ classes/Personagem.ts
@@ -1,45 +1,71 @@
-import Arma from "./Arma"
+import type Arma from "./Arma";
+import type Inimigo from "./Inimigo";
 
 export default class Personagem {
-    private nome: string = ''
-    private vida: number = 100
-    private forca: number = 10
-    private arma: Arma | null = null
+  private nome: string = "";
+  private vida: number = 100;
+  private força: number = 10;
+  private arma: Arma | null = null;
 
-    constructor(nome: string, vida: number = 100, forca: number = 10) {
-        this.nome = nome
-        this.vida = vida
-        this.forca = forca
+  constructor(nome: string, vida: number = 100, força: number = 10) {
+    this.nome = nome;
+    this.vida = vida;
+    this.força = força;
+  }
+
+  getNome() {
+    return this.nome;
+  }
+  getVida() {
+    return this.vida;
+  }
+  getForça() {
+    return this.força;
+  }
+  getArma() {
+    return this.arma;
+  }
+
+  setNome(nome: string) {
+    this.nome = nome;
+  }
+  setVida(vida: number) {
+    this.vida = vida;
+  }
+  setForça(força: number) {
+    this.força = força;
+  }
+  setArma(arma: Arma) {
+    this.arma = arma;
+  }
+
+  //metodo para receber dano
+
+  receberDano(dano: number) {
+    this.vida = this.vida - dano;
+    if (this.vida < 0) {
+      console.log("Personagem F");
     }
+  }
+  //metodo para equipar a arma
+  equiparArma(arma: Arma) {
+    this.arma = arma;
+    console.log(this.arma.getNome() + " Equipado");
+  }
 
-    getNome() { return this.nome }
-    getVida() { return this.vida }
-    getForca() { return this.forca }
-    getArma() { return this.arma }
+  //metodo para atacar inimigo
 
-    setNome(nome: string) { this.nome = nome }
-    setVida(vida: number) { this.vida = vida }
-    setForca(forca: number) { this.forca = forca }
-    setArma(arma: Arma) { this.arma = arma }
+  atacarInimigo(inimigo: Inimigo) {
+    inimigo.receberDano(this.causarDano());
+  }
 
-    receberDano(dano: number) {
-        this.vida = this.vida - dano
-        if (this.vida < 0) {
-            console.log("Personagem F")
-        }
+  //metodo para caucular o dano que o personagem vai causar
+
+  protected causarDano() {
+    if (this.arma === null) {
+      return this.força;
+    } else {
+      return this.força + this.arma.getDano();
     }
-
-    equiparArma(arma: Arma) {
-        this.arma = arma
-        console.log(this.arma.getNome() + ' Equipada')
-    }
-
-    private causarDano() {
-        if (this.arma === null) {
-            return this.forca
-        } else {
-            return this.forca + this.arma.getDano()
-        }
-    }
-
+  }
 }
